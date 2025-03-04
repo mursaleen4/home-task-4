3db9e3bea33fd88c2c948193b084d95209edcbb8
MASHALLAH <MASHALLAH@MUSA-PC>
2025-03-04 12:43:39 +0500

    Add project files.

---
+++ book.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace home_task_4
+{
+
+
+    using System;
+
+    class Book
+    {
+        public string Title;
+        public string Author;
+        public string ISBN;
+        public double Price;
+
+        public void DisplayBookInfo()
+        {
+            Console.WriteLine("Title: " + Title);
+            Console.WriteLine("Author: " + Author);
+            Console.WriteLine("ISBN: " + ISBN);
+            Console.WriteLine("Price: " + Price);
+        }
+
+        public void ApplyDiscount(double percentage)
+        {
+            Price -= Price * (percentage / 100);
+        }
+    }
+}
